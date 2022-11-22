1757bccfc2c95fad592026f2f84176a6acf2783e
CristianVaghetti <vaghetticristian@gmail.com>
2022-11-22 15:49:49 -0300

    Adicionado as views, footer e navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+    <div class="col mx-auto text-center" style="background-color: rgba(0, 0, 0, 0.2);">
+        <img src="https://www.camaracapaodoleao.rs.gov.br/img/header-logo.png" alt="">
+    </div>
+    <div class="row">
+        <div class="col"></div>
+        <div class="col">
+            <nav class="navbar navbar-expand-sm bg-light navbar-dark">
+                <ul class="navbar-nav">
+                    <li class="nav-item m-1">
+                        <router-link class="btn btn-light btn-outline-primary" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item m-1">
+                        <router-link class="btn btn-light btn-outline-primary" to="/fornecedor">Fornededores
+                        </router-link>
+                    </li>
+                    <li class="nav-item m-1">
+                        <router-link class="btn btn-light btn-outline-primary" to="/vereador">Vereadores</router-link>
+                    </li>
+                    <li class="nav-item m-1">
+                        <router-link class="btn btn-light btn-outline-primary" to="/material">Materiais</router-link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="col"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavbarComp'
+}
+</script>
---
+++ src/views/Fornecedor.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Página dos fornecedores</h1>
+</template>
+
+<script>
+export default {
+  name: 'fornecedorView'
+}
+</script>
